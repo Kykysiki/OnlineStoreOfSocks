041eed3528ac5ec595a8326faaf5fa688989fb80
bul.gleb <bul.gleb@gmail.com>
2023-01-26 15:30:40 +0300

    Initial commit

---
+++ src/test/java/me/bulkanov/onlinestoreofsocks/OnlineStoreOfSocksApplicationTests.java
@@ -0,0 +1,13 @@
+package me.bulkanov.onlinestoreofsocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreOfSocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
